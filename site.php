0e07d98b66361b9ed8dc3472280c178d926f98b9
luiscostalafc <cpdbpec2@gmail.com>
2019-04-22 15:31:57 -0300

    Paginação

--- site.php
+++ site.php
@@ -20,16 +20,32 @@ $page->setTpl("index", [
 
 $app->get("/categories/:idcategoy", function($idcategory){
 
+$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
 $category = new Category();
 
 $category->get((int)$idcategory);
 
+$pagination = $category->getProductsPage($page);
+
+$pages = [];
+
+for ($i=1; $i <= $pagination['pages']; $i++) {
+	array_push($pages, [
+
+ 	'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+ 	'page'=>$i
+
+	]);
+
+}
+
 $page = new Page();
 
 $page->setTpl("category", [
 'category'=>$category->getValues(),
-'products'=>Product::checkList($category->getProducts())
+'products'=>$pagination["data"],
+'pages'=>$pages
 
 ]);
 

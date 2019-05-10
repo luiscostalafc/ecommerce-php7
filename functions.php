6a8c725a06e5226f703a5c5dcff8df5d9d47c614
luiscostalafc <cpdbpec2@gmail.com>
2019-05-10 16:26:32 -0300

    Admin pedidos

--- functions.php
+++ functions.php
@@ -12,6 +12,12 @@ return number_format($vlprice, 2, ",", ".");
 
 }
 
+function formatDate($date){
+
+	return date('d/m/Y', strtotime($date));
+
+}
+
 function checkLogin($inadmin = true){
 
 return User::checkLogin($inadmin);
--- index.php
+++ index.php
@@ -16,6 +16,8 @@ require_once("admin.php");
 require_once("admin-users.php");
 require_once("admin-categories.php");
 require_once("admin-products.php");
+require_once("admin-orders.php");
+
 
 $app->run();
 

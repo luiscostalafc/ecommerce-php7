921c7657880dafacc303611bf21c23d8ed3a1a73
luiscostalafc <cpdbpec2@gmail.com>
2019-05-13 12:23:25 -0300

    Admin - Usuário ajustes

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/3">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/5">Motorola</a></li><li><a href="/categories/6">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/8">Android</a></li><li><a href="/categories/5">Motorola</a></li><li><a href="/categories/6">Samsung</a></li>
